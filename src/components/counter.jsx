80cbb3693a8b47ad5f75b94ed889ebeaf1d39b5e
attackpiggy <s.yusuf91@hotmail.com>
2019-12-27 20:10:39 -0500

    inintial change

---
+++ src/components/counter.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class counter extends Component {
+   
+    render() { 
+        return ( 
+
+            <div>This is a headline </div>
+         );
+    }
+}
+ 
+export default counter;
