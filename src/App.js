f2e7de0ee38742e626d7135341cc806b9bf402f8
attackpiggy <s.yusuf91@hotmail.com>
2019-12-28 12:40:19 -0500

    Cleaning for vidly

--- src/App.js
+++ src/App.js
@@ -6,25 +6,9 @@ import Counter from "./components/counter"
 function App() {
   return (
   
-    <div className="App">
-      
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      
-    </div>
+    <main className="container">
+      <h1>This is anotherHEADLINE</h1>
+    </main>
   );
 }
 
--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 class counter extends Component {
     state={
         count:0,
-        tags:['tag1','tag2','tag3']
+        
     }
  
     render() {
