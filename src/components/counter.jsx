e19b10478472b742a9c6cd1335e46239fb5ecaa2
attackpiggy <s.yusuf91@hotmail.com>
2019-12-27 22:59:43 -0500

    practise

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
 class counter extends Component {
+    state={
+        count:0,
+        
+    }
+    formatCount(){
+        const {count} =this.state;
+        return count===0 ? "Zero" :count
+    }
+    styles={
+        fontSize:50,
+        fontWeight:"bold"
+
+    };
    
     render() { 
         return ( 
 
-            <div>This is a headline </div>
+            <div>
+                
+                <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()} </span> 
+                <button style={this.styles}className="btn btn-secondary btn-sm">Increment</button>
+            </div>
          );
     }
 }
