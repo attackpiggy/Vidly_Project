2f1a0fe316da8377c126b291f5f4ec908ab87e85
attackpiggy <s.yusuf91@hotmail.com>
2019-12-28 13:02:46 -0500

    Clearing up the file

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from "./components/counter"
+import Movies from "./components/movie"
 
 function App() {
   return (
   
     <main className="container">
-      <h1>This is anotherHEADLINE</h1>
+      <Movies/>
     </main>
   );
 }
---
+++ src/components/movie.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Movies extends Component {
+    state = {  }
+    render() { 
+        return (  
+            <div>this is the movie component</div>
+        );
+    }
+}
+ 
+export default Movies;
