ffde840dd68810d04134151104eed46f2cd46219
attackpiggy <s.yusuf91@hotmail.com>
2020-01-13 22:02:06 -0500

    fixing up the counter component so that it can be called into other functions

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,6 +2,16 @@ import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 
 class Counter extends Component {
+
+ state={
+   count:0,
+   //tags:['tag1','tag2','tag3']
+ }
+ handleIncrement=()=>{
+  
+   this.setState({count: this.state.count+1 })
+ }
+
     render() {
       console.log("props", this.props);
       const { counter, onIncrement, onDecrement, onDelete } = this.props;
@@ -9,7 +19,7 @@ class Counter extends Component {
         <div className="row">
           <div className="col-1">
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          </div>
+          </div> 
           <div className="col">
             <button
               onClick={() => onIncrement(counter)}
@@ -34,10 +44,10 @@ class Counter extends Component {
         </div>
       );
     }
-  
+   
     getBadgeClasses() {
       let classes = "badge m-2 badge-";
-      classes += this.props.counter.value === 0 ? "warning" : "primary";
+      classes += this.state.count === 0 ? "warning" : "primary";
       return classes;
     }
   
@@ -45,6 +55,7 @@ class Counter extends Component {
       const { value } = this.props.counter;
       return value === 0 ? "Zero" : value;
     }
+
   }
   
 export default Counter;
