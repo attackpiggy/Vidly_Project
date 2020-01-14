e937b4f287374b0f01e7066c75bab907536c9783
attackpiggy <s.yusuf91@hotmail.com>
2020-01-13 20:37:08 -0500

    composing multiple component

---
+++ src/components/counters.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Counters extends Component {
+    
+    render() { 
+        return ( <h1>This is another statment</h1> );
+    }
+}
+ 
+export default Counters;
