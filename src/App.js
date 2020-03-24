917fbf82fdec4734bcfde124cc95f9b863b537f0
attackpiggy <s.yusuf91@hotmail.com>
2020-03-24 13:18:05 -0400

    Adding more components and testing for proper sync

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
-
+import NavBar from './components/navbar'
 import './App.css';
 import Counter from "./components/counter"
-import Movies from "./components/movies"
+//import Movies from "./components/movies"
 
-function App() {
-  return (
+// function App() {
+//   return (
   
-    <main className="container">
-      <Movies/>
-    </main>
-  );
-}
+//     <main className="container">
+//       <Movies/>
+//     </main>
+//   );
+// }
 
+// export default App;
+
+
+
+class App extends Component {
+  
+  render() { 
+    return (  );
+  }
+}
+ 
 export default App;
