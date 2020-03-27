bf1d2174845436c20988f4cf9eafbe7a8f63df25
attackpiggy <s.yusuf91@hotmail.com>
2020-03-26 22:56:12 -0400

    resdesigning the state

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,43 +5,6 @@ import Counter from './counter'
 
 class Counters extends Component {
 
-   state={
-       counters:[
-        {id:1, value:0},
-        {id:2, value:0},
-        {id:3, value:0},
-        {id:4, value:0}
-       ]
-   } 
-
-
-// need to work on this part a bit more, plumbing apparently working not sure why though.!!
-   handleIncrement=counter=>{
-     console.log(counter, "plumbing check ")
-     const counters=[...this.state.counters]
-     const index = counters.indexOf(counter)
-     counters[index]={...counter}
-     counters[index].value++
-     this.setState({counters})
-   }
-
-   handleDelete= counterId =>{
-    //console.log("Testing Event handler", counterId)
-
-    const counters=this.state.counters.filter(c=>c.id !== counterId)
-    this.setState({counters})
-
-   }
-
-handleReset=()=>{
-  const counters=this.state.counters.map(c=>{
-    c.value=0
-    return c
-  })
-  this.setState({counters})
-}
-
-
 
 
 
