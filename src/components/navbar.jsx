571bef9561e33f412798882a6f25d32470536f13
attackpiggy <s.yusuf91@hotmail.com>
2020-04-02 01:37:11 -0400

    destructuring some codes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,14 +2,14 @@ import React, { Component } from 'react'
 
 //functional component
 
-const NavBar=( props)=>{
+const NavBar=( {totalCounters})=>{
 
     return ( 
         <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
     Navbar{" "} 
     
-    <span className="badge badge-pill badge-scondary">{props.totalCounters}</span>
+    <span className="badge badge-pill badge-scondary">{totalCounters}</span>
         </a>
         </nav>
     
