26fcb9eae235e1659f7209aa3f36a75b2da3f22a
attackpiggy <s.yusuf91@hotmail.com>
2020-04-01 22:35:52 -0400

    fixing up the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,7 +8,7 @@ class NavBar extends Component {
         return ( 
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#">Navbar</a>
-        Navbar <span className="badge-pill badge-sco">{this.props.totalCoounters}</span>
+        Navbar{" "} <span className="badge-pill badge-scondary">{this.props.totalCounters}</span>
             </nav>
 
 
