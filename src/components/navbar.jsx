02d83fb49c751861d8fa9e1e6161a7144c68f02c
attackpiggy <s.yusuf91@hotmail.com>
2020-04-01 22:33:55 -0400

    adding counter to the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -8,6 +8,7 @@ class NavBar extends Component {
         return ( 
             <nav className="navbar navbar-light bg-light">
                 <a className="navbar-brand" href="#">Navbar</a>
+        Navbar <span className="badge-pill badge-sco">{this.props.totalCoounters}</span>
             </nav>
 
 
