7db7e6bc9ed2141bfcfb4f3a69791f69237e773d
attackpiggy <s.yusuf91@hotmail.com>
2020-04-05 23:38:32 -0400

    adding a pagination component

---
+++ src/components/pagination.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+
+class Pagination extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  <div>
+
+                    <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                        <li className="page-item"><a className="page-link" href="#">1</a></li>
+                        <li className="page-item"><a className="page-link" href="#">2</a></li>
+                        <li className="page-item"><a className="page-link" href="#">3</a></li>
+                        <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                    </ul>
+                    </nav>
+
+            
+
+
+
+
+
+        </div>);
+    }
+}
+ 
+export default Pagination;
