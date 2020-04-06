54609bb3f74144669c169b46a77f9e53dc238761
attackpiggy <s.yusuf91@hotmail.com>
2020-04-06 12:55:34 -0400

    fixing up the pagination component

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -4,7 +4,12 @@ import React, { Component } from 'react'
 
 
 const Pagination=(props)=>{
-    return null;
+    return <nav>
+        <ul className="pagination">
+            <li className="page-item">
+                <a  className="page-link">1</a></li>
+        </ul>
+    </nav>;
 }
 // class Pagination extends Component {
 //     constructor(props) {
