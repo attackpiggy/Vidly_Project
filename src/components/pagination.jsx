89bad35c966e20e77e32a5a6cdc0ee4b06793bfe
attackpiggy <s.yusuf91@hotmail.com>
2020-04-06 23:59:34 -0400

    minor updates

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -6,6 +6,7 @@ import _ from 'lodash'
 const Pagination=props=>{
     const {itemsCount,pageSize}=props
     const pagesCount=itemsCount /pageSize
+    if (pagesCount === 1) return null;
     const pages= _.range(1,pagesCount+1)
 
 
