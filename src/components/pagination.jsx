bb584abf99041b22d07a1b604814bdd80e272004
attackpiggy <s.yusuf91@hotmail.com>
2020-04-06 23:45:08 -0400

    fixing the pagination

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
-
+import _ from 'lodash'
 
 
 
 const Pagination=props=>{
     const {itemsCount,pageSize}=props
+    const pagesCount=itemsCount /pageSize
+    const pages= _.range(1,pagesCount+1)
+
 
     return <nav>
         <ul className="pagination">
-            <li className="page-item">
-                <a  className="page-link">1</a></li>
+            {pages.map(page=> <li className="page-item">
+                <a  className="page-link">1</a>
+            </li>
+            )}
+           
         </ul>
     </nav>;
 }
