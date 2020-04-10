79e0fcb2d2906d2d832313a8a890133ad56e31d2
attackpiggy <s.yusuf91@hotmail.com>
2020-04-10 09:12:45 -0400

    adding error checking

--- src/components/pagination.jsx
+++ src/components/pagination.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import _ from 'lodash'
+import PropTypes from 'prop-types'
 
 
 
@@ -25,4 +26,9 @@ const Pagination=props=>{
     </nav>;
 }
 
+Pagination.propTypes={
+    itemsCount:PropTypes.number.isRequired,pageSize:PropTypes.number.isRequired,onPageChange:PropTypes.func.isRequired,currentPage:PropTypes
+}
+
 export default Pagination;
+ 
