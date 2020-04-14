35e46dec91513f0c1c0edcac2d006e4860dc7278
attackpiggy <s.yusuf91@hotmail.com>
2020-04-13 22:24:50 -0400

    creating an interface

---
+++ src/components/listGroup.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+
+class ListGroup extends Component {
+    state = {  }
+    render() { 
+        return null;
+    }
+}
+ 
+export default ListGroupnull
