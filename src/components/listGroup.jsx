4267ea22069a192293bea9760f7676624de1ac59
attackpiggy <s.yusuf91@hotmail.com>
2020-04-13 22:48:42 -0400

    adding some more features

--- src/components/listGroup.jsx
+++ src/components/listGroup.jsx
@@ -5,7 +5,7 @@ const ListGroup=(props)=> {
     const {items}=props
 
     return <ul className="list-group">
-        {items.map(item=><li key={item._id}className="list-group-item"></li>)}
+        {items.map(item=><li key={item._id}className="list-group-item">{item.name}</li>)}
         
     </ul>
 
