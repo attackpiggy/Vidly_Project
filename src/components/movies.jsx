239ec5eb3f6794dbdf207a39a65129ef2a981a33
attackpiggy <s.yusuf91@hotmail.com>
2020-04-14 21:38:42 -0400

    fixing up the fileter inteface

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -42,8 +42,8 @@ class Movies extends Component {
             const movies = paginate (allMovies,currentPage,pageSize) 
         return (
          <div className="row">
-             <div className="col-2">
-                 <ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect}/>
+             <div className="col-3">
+                 <ListGroup items={this.state.genres} onItemSelect={this.handleGenreSelect} textPropert="name" valueProperty="_id" />
              </div>
              
              
