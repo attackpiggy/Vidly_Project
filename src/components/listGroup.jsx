74a27a3e36830ab62d1e0fbf915d6e657d1397bd
attackpiggy <s.yusuf91@hotmail.com>
2020-04-14 21:40:23 -0400

    minor changes

--- src/components/listGroup.jsx
+++ src/components/listGroup.jsx
@@ -2,10 +2,10 @@ import React, { Component } from 'react';
 
 
 const ListGroup=(props)=> {
-    const {items}=props
+    const {items,textProperty,valueProperty}=props
 
     return <ul className="list-group">
-        {items.map(item=><li key={item._id}className="list-group-item">{item.name}</li>)}
+        {items.map(item=><li key={item[valueProperty]}className="list-group-item">{item[textProperty]}</li>)}
         
     </ul>
 
